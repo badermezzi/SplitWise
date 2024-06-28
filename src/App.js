7924ef0b1a9b5bf193ef4ce16e02885e6aff63d6
bader <mezzibader159@gmail.com>
2024-06-28 22:57:47 +0100

    Update state to hide SPLIT A BILL form after submission

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ export default function App() {
 
     setfriends(friends => friends.map((friend) => (friend?.id === selectedFriend?.id ? { ...friend, balance: friend.balance + value } : friend)));
 
+    setSelectedFriend(null)
 
   }
 
